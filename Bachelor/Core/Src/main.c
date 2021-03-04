1cba2b2bf90299770b572be66782fd4d23d721fc
jpneb <jpneb@192.168.0.5>
2021-03-04 12:56:07 +0100

    dette er en prøve

--- Bachelor/Core/Src/main.c
+++ Bachelor/Core/Src/main.c
@@ -66,7 +66,7 @@ static void MX_ADC1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	// Dette er en test
+	// Dette er en test, fra JP
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
