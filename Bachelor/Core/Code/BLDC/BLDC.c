17d2a450d3c33a15e8f21dd42588ad9a7249bebc
kenne <kenne@Kennytrez>
2021-03-22 23:27:07 +0100

    Update pseudo code with strategy for each case

--- Bachelor/Core/Code/BLDC/BLDC.c
+++ Bachelor/Core/Code/BLDC/BLDC.c
@@ -28,149 +28,149 @@ void pfx_BLDC() // Main function for commutation
 {
 	f_dutyCap = pfx_Ramp(motorpos.uint8_scheme);
 
-	if(motorpos.uint8_scheme == 1) // 6 Commutation Strategy
+	if(motorpos.uint8_scheme == 1) // 6 Commutation Strategy (Outgoing PWM)
 		{
 			switch(motorpos.uint8_position)
 				{
 					case 1 : // 30-60 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Full On
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - PWM
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 2 : // 60-90 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Full On
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - PWM
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 3 : // 90-120 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - PWM
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Full On
 					 	}
 					break;
 
 					case 4 : // 120-150 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - PWM
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Full On
 					 	}
 					break;
 
 					case 5 : // 150-180 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Full On
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - PWM
 					 	}
 					break;
 
 					case 6 : // 180-210 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Full On
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - PWM
 					 	}
 					break;
 
 					case 7 : // 210-240 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Full On
+					 		//Timer1Ch2 signal 	- Q3 - PWM
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 8 : // 240-270 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Full On
+					 		//Timer1Ch2 signal 	- Q3 - PWM
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 9 : // 270-300 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - PWM
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Full On
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 10 : // 300-330 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - PWM
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Full On
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 11 : // 330-360 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Full On
+					 		//Timer1Ch3 signal 	- Q5 - PWM
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 12 : // 0-30 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Full On
+					 		//Timer1Ch3 signal 	- Q5 - PWM
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					default :
-							//Timer1Ch1 = 5000; (Turned off)
-							//Timer1Ch1N = 5000; (Turned off)
-							//Timer1Ch2 = 5000; (Turned off)
-							//Timer1Ch2N = 5000; (Turned off)
-							//Timer1Ch3 = 5000; (Turned off)
-							//Timer1Ch3N = 5000; (Turned off)
+							//Timer1Ch1 	= 5000;	(Turned off)
+							//Timer1Ch1N 	= 5000; (Turned off)
+							//Timer1Ch2 	= 5000; (Turned off)
+							//Timer1Ch2N 	= 5000; (Turned off)
+							//Timer1Ch3 	= 5000; (Turned off)
+							//Timer1Ch3N 	= 5000; (Turned off)
 				}
 		}
 	else if(motorpos.uint8_scheme ==2) // 12 Commutation Strategy
@@ -179,143 +179,143 @@ void pfx_BLDC() // Main function for commutation
 				{
 					case 1 : // 30-60 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - PWM
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Full on
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 2 : // 60-90 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Full On
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - PWM
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 3 : // 90-120 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Full On
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - PWM
 					 	}
 					break;
 
 					case 4 : // 120-150 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - PWM
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Full On
 					 	}
 					break;
 
 					case 5 : // 150-180 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - PWM
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Full On
 					 	}
 					break;
 
 					case 6 : // 180-210 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Full On
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - PWM
 					 	}
 					break;
 
 					case 7 : // 210-240 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - PWM
+					 		//Timer1Ch2 signal 	- Q3 - Full On
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 8 : // 240-270 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Full On
+					 		//Timer1Ch2 signal 	- Q3 - PWM
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Turned Off
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 9 : // 270-300 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Full On
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - PWM
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 10 : // 300-330 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - PWM
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Turned Off
+					 		//Timer1Ch3 signal 	- Q5 - Full On
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 11 : // 330-360 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - PWM
+					 		//Timer1Ch3 signal 	- Q5 - Full On
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					case 12 : // 0-30 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1
-					 		//Timer1Ch1N signal - Q4
-					 		//Timer1Ch2 signal 	- Q3
-					 		//Timer1Ch2N signal - Q6
-					 		//Timer1Ch3 signal 	- Q5
-					 		//Timer1Ch3N signal - Q2
+					 		//Timer1Ch1 signal 	- Q1 - Turned Off
+					 		//Timer1Ch1N signal - Q4 - Turned Off
+					 		//Timer1Ch2 signal 	- Q3 - Turned Off
+					 		//Timer1Ch2N signal - Q6 - Full On
+					 		//Timer1Ch3 signal 	- Q5 - PWM
+					 		//Timer1Ch3N signal - Q2 - Turned Off
 					 	}
 					break;
 
 					default :
-							//Timer1Ch1 = 5000; (Turned off)
-							//Timer1Ch1N = 5000; (Turned off)
-							//Timer1Ch2 = 5000; (Turned off)
-							//Timer1Ch2N = 5000; (Turned off)
-							//Timer1Ch3 = 5000; (Turned off)
-							//Timer1Ch3N = 5000; (Turned off)
+							//Timer1Ch1 	= 5000; (Turned off)
+							//Timer1Ch1N 	= 5000; (Turned off)
+							//Timer1Ch2 	= 5000; (Turned off)
+							//Timer1Ch2N 	= 5000; (Turned off)
+							//Timer1Ch3 	= 5000; (Turned off)
+							//Timer1Ch3N 	= 5000; (Turned off)
 				}
 		}
 }
