2c2d04ec488bb821c98c9687ff5fb1afce9d955c
kenne <kenne@Kennytrez>
2021-03-22 15:40:32 +0100

    Corrected MotorPos

---
+++ Bachelor/Core/Code/MotorPosition/MotorPos.h
@@ -0,0 +1,13 @@
+/*
+ * MotorPos.h
+ *
+ *  Created on: Mar 22, 2021
+ *      Author: kenne
+ */
+
+#ifndef CODE_MOTORPOSITION_MOTORPOS_H_
+#define CODE_MOTORPOSITION_MOTORPOS_H_
+
+
+
+#endif /* CODE_MOTORPOSITION_MOTORPOS_H_ */
