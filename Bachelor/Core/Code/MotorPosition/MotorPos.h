6a6e37fd58e28d92cbb1e62afb3e7e6e20470348
jpneb <jpneb@192.168.0.5>
2021-03-25 15:47:27 +0100

    */ removed

--- Bachelor/Core/Code/MotorPosition/MotorPos.h
+++ Bachelor/Core/Code/MotorPosition/MotorPos.h
@@ -11,5 +11,5 @@
 struct ST_MOTORPOS{uint8_t uint8_position;uint8_t uint8_scheme;uint8_t uint8_positionOld;}motorpos;
 
 struct ST_MOTORPOS pfx_MotorPos();
-*/
+
 #endif /* CODE_MOTORPOSITION_MOTORPOS_H_ */
