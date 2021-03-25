d3f0606ef35bf74b34df2ca550893e50fdf19bb1
kenne <kenne@Kennytrez>
2021-03-25 09:43:32 +0100

    Updated .h files

---
+++ Bachelor/Core/Code/BLDC/BLDC.h
@@ -0,0 +1,14 @@
+/*
+ * BLDC.h
+ *
+ *  Created on: Mar 22, 2021
+ *      Author: Kenneth Meier Jensen
+ */
+
+#ifndef CODE_BLDC_BLDC_H_
+#define CODE_BLDC_BLDC_H_
+
+void pfx_BLDC();
+
+
+#endif /* CODE_BLDC_BLDC_H_ */
--- Bachelor/Core/Code/PWM/PWM.h
+++ Bachelor/Core/Code/PWM/PWM.h
@@ -8,6 +8,7 @@
 #ifndef CODE_PWM_PWM_H_
 #define CODE_PWM_PWM_H_
 
-
+void pfx_PWM_Stop();
+void pfx_PWM(float f_dutyCycle, uint8_t uint8_timerChannel);
 
 #endif /* CODE_PWM_PWM_H_ */
