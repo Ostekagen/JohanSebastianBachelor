94b5c6e9f29a696516e390e390479f464cc41859
kenne <kenne@Kennytrez>
2021-03-26 22:44:10 +0100

    Created BLDC code and cleaned main from changes

--- Bachelor/Core/Code/BLDC/BLDC.c
+++ Bachelor/Core/Code/BLDC/BLDC.c
@@ -10,8 +10,7 @@
 #include <math.h>
 #include "stdio.h"
 #include <stdint.h>
-//#include "stm32f4xx_hal_tim.h"	//Laver fejl i drivers
-#include "BLDC.h"
+#include "PWM.h"
 #include "Ramp.h"
 #include "MotorPos.h"
 #include "main.h"
@@ -33,144 +32,116 @@ void pfx_BLDC() // Main function for commutation
 				{
 					case 1 : // 30-60 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Full On
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - PWM
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFAEB; 	// Turn off Q4 & Q3 & Q5 & Q2
+					 		TIM1->CCER |= 0x41;		// Turn on Q1 & Q6
+					 		pfx_PWM(0, 1);			// CH1 - Q1 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2N - Q6 - PWM
 					 	}
 					break;
 
 					case 2 : // 60-90 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Full On
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - PWM
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFAEB; 	// Turn off Q4 & Q3 & Q5 & Q2
+					 		TIM1->CCER |= 0x41;		// Turn on Q1 & Q6
+					 		pfx_PWM(0, 1);			// CH1 - Q1 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2N - Q6 - PWM
 					 	}
 					break;
 
 					case 3 : // 90-120 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - PWM
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Full On
+					 		TIM1->CCER &= 0xFEAB; 	// Turn off Q4 & Q3 & Q6 & Q5
+					 		TIM1->CCER |= 0x401;	// Turn on Q1 & Q2
+					 		pfx_PWM(f_dutyCap, 1);	// CH1 - Q1 - PWM
+					 		pfx_PWM(0, 3); 			// CH3N - Q2 - Full On
 					 	}
 					break;
 
 					case 4 : // 120-150 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - PWM
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Full On
+					 		TIM1->CCER &= 0xFEAB; 	// Turn off Q4 & Q3 & Q6 & Q5
+					 		TIM1->CCER |= 0x401;	// Turn on Q1 & Q2
+					 		pfx_PWM(f_dutyCap, 1);	// CH1 - Q1 - PWM
+					 		pfx_PWM(0, 3); 			// CH3N - Q2 - Full On
 					 	}
 					break;
 
 					case 5 : // 150-180 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Full On
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - PWM
+					 		TIM1->CCER &= 0xFEBA; 	// Turn off Q1 & Q4 & Q6 & Q5
+					 		TIM1->CCER |= 0x410;	// Turn on Q3 & Q2
+					 		pfx_PWM(0, 2);			// CH2 - Q3 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3N - Q2 - PWM
 					 	}
 					break;
 
 					case 6 : // 180-210 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Full On
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - PWM
+					 		TIM1->CCER &= 0xFEBA; 	// Turn off Q1 & Q4 & Q6 & Q5
+					 		TIM1->CCER |= 0x410;	// Turn on Q3 & Q2
+					 		pfx_PWM(0, 2);			// CH2 - Q3 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3N - Q2 - PWM
 					 	}
 					break;
 
 					case 7 : // 210-240 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Full On
-					 		//Timer1Ch2 signal 	- Q3 - PWM
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q6 & Q5 & Q2
+					 		TIM1->CCER |= 0x14;		// Turn on Q4 & Q3
+					 		pfx_PWM(0, 1);			// CH1N - Q4 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2 - Q3 - PWM
 					 	}
 					break;
 
 					case 8 : // 240-270 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Full On
-					 		//Timer1Ch2 signal 	- Q3 - PWM
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q6 & Q5 & Q2
+					 		TIM1->CCER |= 0x14;		// Turn on Q4 & Q3
+					 		pfx_PWM(0, 1);			// CH1N - Q4 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2 - Q3 - PWM
 					 	}
 					break;
 
 					case 9 : // 270-300 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - PWM
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Full On
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q3 & Q6 & Q2
+					 		TIM1->CCER |= 0x104;	// Turn on Q4 & Q5
+					 		pfx_PWM(f_dutyCap, 1);	// CH1N - Q4 - PWM
+					 		pfx_PWM(0, 3);			// CH3 - Q5 - Full On
 					 	}
 					break;
 
 					case 10 : // 300-330 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - PWM
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Full On
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q3 & Q6 & Q2
+					 		TIM1->CCER |= 0x104;	// Turn on Q4 & Q5
+					 		pfx_PWM(f_dutyCap, 1);	// CH1N - Q4 - PWM
+					 		pfx_PWM(0, 3);			// CH3 - Q5 - Full On
 					 	}
 					break;
 
 					case 11 : // 330-360 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Full On
-					 		//Timer1Ch3 signal 	- Q5 - PWM
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFBEA; 	// Turn off Q1 & Q4 & Q3 & Q2
+					 		TIM1->CCER |= 0x140;	// Turn on Q6 & Q5
+					 		pfx_PWM(0, 2);			// CH2N - Q6 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3 - Q5 - PWM
 					 	}
 					break;
 
 					case 12 : // 0-30 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Full On
-					 		//Timer1Ch3 signal 	- Q5 - PWM
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFBEA; 	// Turn off Q1 & Q4 & Q3 & Q2
+					 		TIM1->CCER |= 0x140;	// Turn on Q6 & Q5
+					 		pfx_PWM(0, 2);			// CH2N - Q6 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3 - Q5 - PWM
 					 	}
 					break;
 
 					default :
 					{
-						//Timer1Ch1 	= 5000;	(Turned off)
-						//Timer1Ch1N 	= 5000; (Turned off)
-						//Timer1Ch2 	= 5000; (Turned off)
-						//Timer1Ch2N 	= 5000; (Turned off)
-						//Timer1Ch3 	= 5000; (Turned off)
-						//Timer1Ch3N 	= 5000; (Turned off)
+						TIM1->CCER &= 0xFAAA; 		// Turn off Q1 & Q4 & Q3 & Q6 & Q5 & Q2
+						pfx_PWM_Stop();				// Turn off all PWM by applying compare value larger than counter
 					}
 
 				}
@@ -181,144 +152,116 @@ void pfx_BLDC() // Main function for commutation
 				{
 					case 1 : // 30-60 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - PWM
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Full on
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFAEB; 	// Turn off Q4 & Q3 & Q5 & Q2
+					 		TIM1->CCER |= 0x41;		// Turn on Q1 & Q6
+					 		pfx_PWM(f_dutyCap, 1);	// CH1 - Q1 - PWM
+					 		pfx_PWM(0, 2);			// CH2N - Q6 - Full on
 					 	}
 					break;
 
 					case 2 : // 60-90 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Full On
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - PWM
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFAEB; 	// Turn off Q4 & Q3 & Q5 & Q2
+					 		TIM1->CCER |= 0x41;		// Turn on Q1 & Q6
+					 		pfx_PWM(0, 1);			// CH1 - Q1 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2N - Q6 - PWM
 					 	}
 					break;
 
 					case 3 : // 90-120 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Full On
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - PWM
+					 		TIM1->CCER &= 0xFEAB; 	// Turn off Q4 & Q3 & Q6 & Q5
+					 		TIM1->CCER |= 0x401;	// Turn on Q1 & Q2
+					 		pfx_PWM(0, 1);			// CH1 - Q1 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3N - Q2 - PWM
 					 	}
 					break;
 
 					case 4 : // 120-150 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - PWM
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Full On
+					 		TIM1->CCER &= 0xFEAB; 	// Turn off Q4 & Q3 & Q6 & Q5
+					 		TIM1->CCER |= 0x401;	// Turn on Q1 & Q2
+					 		pfx_PWM(f_dutyCap, 1);	// CH1 - Q1 - PWM
+					 		pfx_PWM(0, 3);			// CH3N - Q2 - Full On
 					 	}
 					break;
 
 					case 5 : // 150-180 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - PWM
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Full On
+					 		TIM1->CCER &= 0xFEBA; 	// Turn off Q1 & Q4 & Q6 & Q5
+					 		TIM1->CCER |= 0x410;	// Turn on Q3 & Q2
+					 		pfx_PWM(f_dutyCap, 2);	// CH2 - Q3 - PWM
+					 		pfx_PWM(0, 3);			// CH3N - Q2 - Full On
 					 	}
 					break;
 
 					case 6 : // 180-210 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Full On
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - PWM
+					 		TIM1->CCER &= 0xFEBA; 	// Turn off Q1 & Q4 & Q6 & Q5
+					 		TIM1->CCER |= 0x410;	// Turn on Q3 & Q2
+					 		pfx_PWM(0, 2);			// CH2 - Q3 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3N - Q2 - PWM
 					 	}
 					break;
 
 					case 7 : // 210-240 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - PWM
-					 		//Timer1Ch2 signal 	- Q3 - Full On
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q6 & Q5 & Q2
+					 		TIM1->CCER |= 0x14;		// Turn on Q4 & Q3
+					 		pfx_PWM(f_dutyCap, 1);	// CH1N - Q4 - PWM
+					 		pfx_PWM(0, 2);			// CH2 - Q3 - Full On
 					 	}
 					break;
 
 					case 8 : // 240-270 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Full On
-					 		//Timer1Ch2 signal 	- Q3 - PWM
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Turned Off
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q6 & Q5 & Q2
+					 		TIM1->CCER |= 0x14;		// Turn on Q4 & Q3
+					 		pfx_PWM(0, 1);			// CH1N - Q4 - Full On
+					 		pfx_PWM(f_dutyCap, 2);	// CH2 - Q3 - PWM
 					 	}
 					break;
 
 					case 9 : // 270-300 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Full On
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - PWM
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q3 & Q6 & Q2
+					 		TIM1->CCER |= 0x104;	// Turn on Q4 & Q5
+					 		pfx_PWM(0, 1);			// CH1N - Q4 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3 - Q5 - PWM
 					 	}
 					break;
 
 					case 10 : // 300-330 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - PWM
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Turned Off
-					 		//Timer1Ch3 signal 	- Q5 - Full On
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFABE; 	// Turn off Q1 & Q3 & Q6 & Q2
+					 		TIM1->CCER |= 0x104;	// Turn on Q4 & Q5
+					 		pfx_PWM(f_dutyCap, 1);	// CH1N - Q4 - PWM
+					 		pfx_PWM(0, 3);			// CH3 - Q5 - Full On
 					 	}
 					break;
 
 					case 11 : // 330-360 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - PWM
-					 		//Timer1Ch3 signal 	- Q5 - Full On
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFBEA; 	// Turn off Q1 & Q4 & Q3 & Q2
+					 		TIM1->CCER |= 0x140;	// Turn on Q6 & Q5
+					 		pfx_PWM(f_dutyCap, 2);	// CH2N - Q6 - PWM
+					 		pfx_PWM(0, 3);			// CH3 - Q5 - Full On
 					 	}
 					break;
 
 					case 12 : // 0-30 degrees
 					 	{
-					 		//Timer1Ch1 signal 	- Q1 - Turned Off
-					 		//Timer1Ch1N signal - Q4 - Turned Off
-					 		//Timer1Ch2 signal 	- Q3 - Turned Off
-					 		//Timer1Ch2N signal - Q6 - Full On
-					 		//Timer1Ch3 signal 	- Q5 - PWM
-					 		//Timer1Ch3N signal - Q2 - Turned Off
+					 		TIM1->CCER &= 0xFBEA; 	// Turn off Q1 & Q4 & Q3 & Q2
+					 		TIM1->CCER |= 0x140;	// Turn on Q6 & Q5
+					 		pfx_PWM(0, 2);			// CH2N - Q6 - Full On
+					 		pfx_PWM(f_dutyCap, 3);	// CH3 - Q5 - PWM
 					 	}
 					break;
 
-					default :
+					default : // Turn off PWM
 						{
-							//Timer1Ch1 	= 5000; (Turned off)
-							//Timer1Ch1N 	= 5000; (Turned off)
-							//Timer1Ch2 	= 5000; (Turned off)
-							//Timer1Ch2N 	= 5000; (Turned off)
-							//Timer1Ch3 	= 5000; (Turned off)
-							//Timer1Ch3N 	= 5000; (Turned off)
+							TIM1->CCER &= 0xFAAA; 		// Turn off Q1 & Q4 & Q3 & Q6 & Q5 & Q2
+							pfx_PWM_Stop();				// Turn off all PWM by applying compare value larger than counter
 						}
 				}
 		}
--- Bachelor/Core/Src/main.c
+++ Bachelor/Core/Src/main.c
@@ -148,9 +148,10 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+while (1)
   {
     /* USER CODE END WHILE */
+
 
     /* USER CODE BEGIN 3 */
   }
