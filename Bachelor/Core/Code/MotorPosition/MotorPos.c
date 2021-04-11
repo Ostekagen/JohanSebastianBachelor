9008cdb19c5074c58bdf2dd0472f5c216fe07a8a
jpneb <jpneb@LAPTOP-FSVRH17M.eduroam.net.au.dk>
2021-04-11 10:34:05 +0200

    deleted earlier code that was commented out.

--- Bachelor/Core/Code/MotorPosition/MotorPos.c
+++ Bachelor/Core/Code/MotorPosition/MotorPos.c
@@ -21,14 +21,14 @@ TIM_OC_InitTypeDef sConfigOC;
 /* Variables */
 struct ST_MOTORPOS{uint8_t uint8_position;uint8_t uint8_scheme;}motorpos={0, 1};
 
-uint8_t hal1 = 0;
-uint8_t hal2 = 0;
+uint8_t uint8_hallBLDC1 = 0;
+uint8_t uint8_hallBLDC2 = 0;
 uint8_t hal3 = 0;
 
 uint8_t uint8_positionOld = 0;
 
-uint16_t schemeValue1 = 2500; // TBD
-uint16_t schemeValue2 = 2000; // TBD
+uint16_t int16_hallUpperLimit = 2500; // TBD
+uint16_t int16_hallLowerLimit = 2000; // TBD
 
 uint16_t maxValue = 65535;// timer 6 is a 16-bit counter
 uint16_t timerValue = 0;
@@ -42,39 +42,39 @@ uint16_t sinceLastComm = 0;
 struct ST_MOTORPOS pfx_MotorPos() // main function
 {
 
-hal1 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10); // read value from gpio-pin (5v tolerant)
-hal2 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11);
+uint8_hallBLDC1 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10); // read value from gpio-pin (5v tolerant)
+uint8_hallBLDC2 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11);
 hal3 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12);
 
 /*60 degrees commutation*/
 uint8_positionOld = motorpos.uint8_position; // save old
 
-if(hal1 == 0 && hal2 == 0 && hal3 == 1 && motorpos.uint8_position != 2 ) // see truth table
+if(uint8_hallBLDC1 == 0 && uint8_hallBLDC2 == 0 && hal3 == 1 && motorpos.uint8_position != 2 ) // see truth table
 	{
 	//uint8_positionOld = motorpos.uint8_position; // save old
 	motorpos.uint8_position = 1; // write new
 	}
-else if(hal1 == 1 && hal2 == 0 && hal3 == 1 && motorpos.uint8_position != 4)
+else if(uint8_hallBLDC1 == 1 && uint8_hallBLDC2 == 0 && hal3 == 1 && motorpos.uint8_position != 4)
 	{
 	//uint8_positionOld = motorpos.uint8_position;
 	motorpos.uint8_position = 3;
 	}
-else if(hal1 == 1 && hal2 == 0 && hal3 == 0 && motorpos.uint8_position != 6)
+else if(uint8_hallBLDC1 == 1 && uint8_hallBLDC2 == 0 && hal3 == 0 && motorpos.uint8_position != 6)
 	{
 	//uint8_positionOld = motorpos.uint8_position;
 	motorpos.uint8_position = 5;
 	}
-else if(hal1 == 1 && hal2 == 1 && hal3 == 0 && motorpos.uint8_position != 8)
+else if(uint8_hallBLDC1 == 1 && uint8_hallBLDC2 == 1 && hal3 == 0 && motorpos.uint8_position != 8)
 	{
 	//uint8_positionOld = motorpos.uint8_position;
 	motorpos.uint8_position = 7;
 	}
-else if(hal1 == 0 && hal2 == 1 && hal3 == 0 && motorpos.uint8_position != 10)
+else if(uint8_hallBLDC1 == 0 && uint8_hallBLDC2 == 1 && hal3 == 0 && motorpos.uint8_position != 10)
 	{
 	//uint8_positionOld = motorpos.uint8_position;
 	motorpos.uint8_position = 9;
 	}
-else if(hal1 == 0 && hal2 == 1 && hal3 == 1 && motorpos.uint8_position != 12)
+else if(uint8_hallBLDC1 == 0 && uint8_hallBLDC2 == 1 && hal3 == 1 && motorpos.uint8_position != 12)
 	{
 	//uint8_positionOld = motorpos.uint8_position;
 	motorpos.uint8_position = 11;
@@ -116,11 +116,11 @@ if( motorpos.uint8_position % 2 != 0) // if motorposition is odd incrementation 
 
 /*Scheme selector*/
 
-if(motorpos.uint8_scheme == 2  && sinceLastComm <= schemeValue1) // Change to slow-scheme due to velocity
+if(motorpos.uint8_scheme == 2  && sinceLastComm <= int16_hallUpperLimit) // Change to slow-scheme due to velocity
 {
 	motorpos.uint8_scheme = 1;
 }
-if(sinceLastComm >= schemeValue2) // Change to fast-scheme due to velocity /remember hysteresis
+if(sinceLastComm >= int16_hallLowerLimit) // Change to fast-scheme due to velocity /remember hysteresis
 {
 	motorpos.uint8_scheme = 2;
 }
