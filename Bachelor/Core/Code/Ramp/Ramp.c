727b90615a1100cfb7061eefd33ef52e45ae3d77
Rasmus mikkelsen <Rasmusmikkelsen@hotmail.com>
2021-05-14 12:17:22 +0200

    Removed Scheme from RAMP function

--- Bachelor/Core/Code/Ramp/Ramp.c
+++ Bachelor/Core/Code/Ramp/Ramp.c
@@ -21,69 +21,43 @@ float f_dutyCapRAMP;
 uint16_t int16_throttleCalcRAMP;
 
 /* Internal Variables */
-int counter;
 float f_lastOutput=0.1;
 float f_gain1 = 1.00033f;
 float f_gain2 = 1.001;
-uint16_t throttleTest;
-float John;
+
+
 
 /* Start Code Here */
 
 // The Ramp function will receive the shecme as input. It will then use the output from throttle to calculate a ramp function
-float pfx_ramp(uint8_t uint8_scheme)
+float pfx_ramp()
 	{
 
 		int16_throttleCalcRAMP = pfx_throttle();	// Calling Throttle function
 
 		// Scheme 1
-		if(uint8_scheme == 1)
+		if ((f_lastOutput == 0) && (int16_throttleCalcRAMP > 0))	// To ensure the throttle output only is zero when the throttle is not pulled
 		{
-			if ((f_lastOutput == 0) && (int16_throttleCalcRAMP > 0))	// To ensure the throttle output only is zero when the throttle is not pulled
-			{
-				f_lastOutput = 0.1;
-			}
-			if (f_lastOutput < int16_throttleCalcRAMP)	// When throttle output is higher than our last output, increase our output
-			{
-				f_lastOutput = f_lastOutput*f_gain1;
-			}
-			else if (f_lastOutput >= int16_throttleCalcRAMP)	// When last output is less or equal to the throttle output, set output to throttle output
-			{
-				f_lastOutput = int16_throttleCalcRAMP;
-			}
-			else
-			{
-				f_lastOutput = 0;
-			}
+			f_lastOutput = 0.1;
 		}
-
-		// Scheme 2
-		else if (uint8_scheme == 2)
+		if (f_lastOutput < int16_throttleCalcRAMP)	// When throttle output is higher than our last output, increase our output
 		{
-			if (f_lastOutput < int16_throttleCalcRAMP)
-			{
-				f_lastOutput = f_lastOutput*f_gain2;
-			}
-			else if (f_lastOutput >= int16_throttleCalcRAMP)
-			{
-				f_lastOutput = int16_throttleCalcRAMP;
-			}
-			else
-			{
-				f_lastOutput = 0;
-			}
+			f_lastOutput = f_lastOutput*f_gain1;
+		}
+		else if (f_lastOutput >= int16_throttleCalcRAMP)	// When last output is less or equal to the throttle output, set output to throttle output
+		{
+			f_lastOutput = int16_throttleCalcRAMP;
 		}
 		else
 		{
 			f_lastOutput = 0;
 		}
-		John = pfx_throttle();
-		f_dutyCapRAMP = John/1000;	// set our output to a value between 0 and 1
+
 
 		if (f_dutyCapRAMP > 0.95)
-			{
-				f_dutyCapRAMP = 0.9;
-			}
+		{
+			f_dutyCapRAMP = 0.9;
+		}
 
 		return f_dutyCapRAMP;
 	}
